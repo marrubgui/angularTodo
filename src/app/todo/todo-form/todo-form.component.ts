661efff33655cf74cb91985273019d55603d1d0d
pepe pepe <marian@Mac-mini-de-pepe.local>
2020-04-01 23:12:47 -0300

    initial commit

---
+++ src/app/todo/todo-form/todo-form.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Todo } from '../models/todo';
+import { TodoService } from '../services/todo.service';
+import { DocumentReference } from '@angular/fire/firestore';
+import { TodoViewModel } from '../models/todo-view-model';
+
+@Component({
+  selector: 'app-todo-form',
+  templateUrl: './todo-form.component.html',
+  styleUrls: ['./todo-form.component.sass']
+})
+export class TodoFormComponent implements OnInit {
+
+  todoForm: FormGroup;
+  createMode:boolean;
+  todo: TodoViewModel;
+
+  constructor(private formBuilder: FormBuilder, public activeModal:NgbActiveModal, private todoService:TodoService) { }
+
+  ngOnInit(): void {
+    this.todoForm = this.formBuilder.group({
+      title:['',Validators.required],
+      description:['', Validators.required],
+      done:false
+    });
+  if(!this.createMode){
+    this.loadTodo(this.todo);
+  }
+
+  }
+
+  loadTodo(todo: TodoViewModel){
+    this.todoForm.patchValue(todo);
+  }
+
+  saveTodo() {
+    if(this.todoForm.invalid){
+      return;
+    }
+    if (this.createMode){
+      let todo:Todo = this.todoForm.value;
+      todo.lastModifiedDate = new Date();
+      todo.createdDate = new Date();
+      this.todoService.saveTodo(todo)
+      .then(response => this.handleSuccessfulSaveTodo(response,todo))
+      .catch(err => console.error(err));
+    } else {
+      let todo: TodoViewModel = this.todoForm.value;
+      todo.id = this.todo.id;
+      todo.lastModifiedDate = new Date();
+      this.todoService.editTodo(todo)
+      .then(response => this.handleSuccessfulEditTodo(todo))
+      .catch(err => console.error(err));
+    }
+  }
+
+  handleSuccessfulSaveTodo(response: DocumentReference, todo:Todo) {
+    this.activeModal.dismiss({todo: todo, id: response.id, createMode: true});
+  }
+
+  handleSuccessfulEditTodo(todo: TodoViewModel){
+    this.activeModal.dismiss({todo:todo , id : todo.id, createMode: false})
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBE9mflpW9GQun6NFCiIc-ZwfwJP-1SE8o",
+    authDomain: "app-todo-34b8f.firebaseapp.com",
+    databaseURL: "https://app-todo-34b8f.firebaseio.com",
+    projectId: "app-todo-34b8f",
+    storageBucket: "app-todo-34b8f.appspot.com",
+    messagingSenderId: "905746464694",
+    appId: "1:905746464694:web:d8bbd78a9ac4af7ee7cb17",
+    measurementId: "G-YY2D0964W8"
+  }
 };
 
 /*
