4c8bee6486597baf9446b45667c3410233d7fffa
pepe pepe <marian@Mac-mini-de-pepe.local>
2020-04-02 04:24:17 -0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angulat-todo'));
+app.use(express.static(__dirname + '/dist/angular-todo'));
 
 app.get('/*', function(req,res) {
     
