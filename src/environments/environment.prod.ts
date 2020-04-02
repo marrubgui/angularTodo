14999eb192f3c860f3aa0eb6280f6e6da219059b
pepe pepe <marian@Mac-mini-de-pepe.local>
2020-04-02 02:36:43 -0300

    updates to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBE9mflpW9GQun6NFCiIc-ZwfwJP-1SE8o",
+    authDomain: "app-todo-34b8f.firebaseapp.com",
+    databaseURL: "https://app-todo-34b8f.firebaseio.com",
+    projectId: "app-todo-34b8f",
+    storageBucket: "app-todo-34b8f.appspot.com",
+    messagingSenderId: "905746464694",
+    appId: "1:905746464694:web:d8bbd78a9ac4af7ee7cb17",
+    measurementId: "G-YY2D0964W8"
+  }
 };
